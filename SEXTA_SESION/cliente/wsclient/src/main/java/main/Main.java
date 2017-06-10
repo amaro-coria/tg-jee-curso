483921de0357b8737d18dbb467caf5674c42d6f4
Jorge Amaro <amaro.coria@gmail.com>
2017-06-10 11:48:45 -0500

    cliente ws

---
+++ SEXTA_SESION/cliente/wsclient/src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import invoke.Invoker;
+
+/**
+ * Created by Amaro on 10/06/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Invoker().invoke();
+    }
+}
